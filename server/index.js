f1708c4c3430fe5933fc80ee7df757de9dcf6903
RishabhAgni2 <rishabhagni68107@gmail.com>
2026-03-01 19:01:09 +0530

    update express configuration and route integration

--- server/index.js
+++ server/index.js
@@ -1,20 +1,31 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import connectDB from "./config/db.js";
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
-connectDB();
+
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+const portfolioRoutes = require("./routes/portfolioRoutes");
+
+app.use("/api/portfolio", portfolioRoutes);
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
-  res.send("FinTrack API is running");
+  res.send("FinTrack API Running");
 });
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
