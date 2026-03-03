15389f14ae0941b8b5bb1d8918f82dd3328f493f
RishabhAgni2 <rishabhagni68107@gmail.com>
2026-03-03 19:59:44 +0530

    improved authentication middleware logic

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -10,7 +10,7 @@ exports.protect = async (req, res, next) => {
       req.headers.authorization.startsWith("Bearer")
     ) {
       token = req.headers.authorization.split(" ")[1];
-
+      console.log("Authorization Header:", req.headers.authorization);
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       req.user = await User.findById(decoded.id).select("-password");
