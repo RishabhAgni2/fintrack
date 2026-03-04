f7e14dd975857c06e7474483b4b9371506e14139
RishabhAgni2 <rishabhagni68107@gmail.com>
2026-03-04 22:14:25 +0530

    updated portfolio routes and added auth middleware

--- server/routes/portfolioRoutes.js
+++ server/routes/portfolioRoutes.js
@@ -7,9 +7,9 @@ const {
 } = require("../controllers/portfolioController");
 
 const { protect } = require("../middleware/authMiddleware");
-
+const { getPortfolioSummary } = require("../controllers/portfolioController");
 router.post("/buy", protect, buyStock);
 router.post("/sell", protect, sellStock);
 router.get("/", protect, getPortfolio);
-
+router.get("/summary", protect, getPortfolioSummary);
 module.exports = router;
